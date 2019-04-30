ceb1da4e8c7f1780e494c8c40feb1bdd9c7fc585
Sansern Kanmano <i3bike_se@outlook.com>
2019-04-30 15:52:18 +0700

    add

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'FdaI9iGFV3tbfRGQd3LlENtD9DvazBlw6xzgf8TVgvfayMntk2aIPRrVxJ+eTNxznvSXo/9DRT/o9QSYor6mrshtfma+ZokAgNj7NoW0fAtGa5Wib6JJDuRo1f/YeLUyXNFrIU/un5R/Oe3Mic9engdB04t89/1O/w1cDnyilFU=';
 
 
 $url = 'https://api.line.me/v1/oauth/verify';
