9f39ddb77347b11b2a439bb69128aee46fd9bac3
Sansern Kanmano <i3bike_se@outlook.com>
2019-04-30 15:49:04 +0700

    add

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'FdaI9iGFV3tbfRGQd3LlENtD9DvazBlw6xzgf8TVgvfayMntk2aIPRrVxJ+eTNxznvSXo/9DRT/o9QSYor6mrshtfma+ZokAgNj7NoW0fAtGa5Wib6JJDuRo1f/YeLUyXNFrIU/un5R/Oe3Mic9engdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
